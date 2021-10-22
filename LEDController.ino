d69c01c5710d3c39043fdba54e53c6135708f4b7
TKafoe <kafoe.tom@gmail>
2021-10-22 14:36:05 +0200

    Final version

--- LEDController.ino
+++ LEDController.ino
@@ -1,8 +1,8 @@
 #include <Conceptinetics.h>
 #include <FastLED.h>
 
-#define DMX_START_CHANNEL 100
-#define DMX_SLAVE_CHANNELS 6
+#define DMX_START_CHANNEL 200
+#define DMX_SLAVE_CHANNELS 14
 
 #define NUM_LEDS_CLOCK 344
 #define NUM_LEDS_IC 60
@@ -13,9 +13,7 @@
 #define DATA_PIN_CLOTHES 10
 #define DATA_PIN_CLOCK 9
 
-#define SHIFT 20
-
-unsigned long long t = millis();
+#define SHIFT 12
 
 DMX_Slave dmx_slave(DMX_SLAVE_CHANNELS);
 CRGB clock_leds[NUM_LEDS_CLOCK];
@@ -37,15 +35,14 @@ void loop() {
 }
 
 void set_clock() {
-  uint8_t ch2 = dmx_slave.getChannelValue(2);
-  uint8_t ch3 = dmx_slave.getChannelValue(3);
-  uint8_t r = dmx_slave.getChannelValue(4);
-  uint8_t g = dmx_slave.getChannelValue(5);
-  uint8_t b = dmx_slave.getChannelValue(6);
+  uint8_t ch2 = dmx_slave.getChannelValue(1);
+  uint8_t ch3 = dmx_slave.getChannelValue(2);
+  uint8_t r = dmx_slave.getChannelValue(3);
+  uint8_t g = dmx_slave.getChannelValue(4);
+  uint8_t b = dmx_slave.getChannelValue(5);
 
   int start = ((float)ch2 / (float) 255) * NUM_LEDS_CLOCK;
   int nd = ((float)ch3 / (float) 255) * NUM_LEDS_CLOCK;
-
 
   for (int i = 0; i < NUM_LEDS_CLOCK; i++) {
     int i_shifted = i + SHIFT;
@@ -69,62 +66,34 @@ void set_clock() {
 }
 
 void set_workers() {
-  uint8_t ch1 = dmx_slave.getChannelValue(1);
-
-  switch (ch1) {
-  case 0: {
-    light_ic(false);
-    light_gardener(false);
-    light_office(false);
-    break;
-  }
-  case 1: {
-    light_ic(true);
-    light_gardener(false);
-    light_office(false);
-    break;
-  }
-  case 2: {
-    light_ic(false);
-    light_gardener(true);
-    light_office(false);
-    break;
-  }
-  case 3:
-  default: {
-    light_ic(false);
-    light_gardener(false);
-    light_office(true);
-  }
-  }
+  light_ic();
+  light_gardener();
+  light_office();
 }
 
-void light_gardener(bool on) {
-  for (int i = NUM_LEDS_IC; i < NUM_LEDS_IC + NUM_LEDS_GARDENER; i++) {
-    if (on) {
-      clothes_leds[i] = CRGB::Red;
-    } else {
-      clothes_leds[i] = CRGB::Black;
-    }
-  }
-}
-
-void light_ic(bool on) {
+void light_ic() {
+  uint8_t r = dmx_slave.getChannelValue(6);
+  uint8_t g = dmx_slave.getChannelValue(7);
+  uint8_t b = dmx_slave.getChannelValue(8);
   for (int i = 0; i < NUM_LEDS_IC; i++) {
-    if (on) {
-      clothes_leds[i] = CRGB::Blue;
-    } else {
-      clothes_leds[i] = CRGB::Black;
-    }
+    clothes_leds[i] = CRGB(r, g, b);
   }
 }
 
-void light_office(bool on) {
+void light_gardener() {
+  uint8_t r = dmx_slave.getChannelValue(9);
+  uint8_t g = dmx_slave.getChannelValue(10);
+  uint8_t b = dmx_slave.getChannelValue(11);
+  for (int i = NUM_LEDS_IC; i < NUM_LEDS_IC + NUM_LEDS_GARDENER; i++) {
+    clothes_leds[i] = CRGB(r, g, b);
+  }
+}
+
+void light_office() {
+  uint8_t r = dmx_slave.getChannelValue(12);
+  uint8_t g = dmx_slave.getChannelValue(13);
+  uint8_t b = dmx_slave.getChannelValue(14);
   for (int i = NUM_LEDS_IC + NUM_LEDS_GARDENER; i < NUM_LEDS_IC + NUM_LEDS_GARDENER + NUM_LEDS_OFFICE; i++) {
-    if (on) {
-      clothes_leds[i] = CRGB::Green;
-    } else {
-      clothes_leds[i] = CRGB::Black;
-    }
+    clothes_leds[i] = CRGB(r, g, b);
   }
 }
